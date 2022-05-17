14e18f0af3b482de14d155c13ab2cc3db4607849
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-17 20:31:41 +0300

    Add tests for Patient model class setter methods

--- src/test/java/models/PatientTest.java
+++ src/test/java/models/PatientTest.java
@@ -38,4 +38,46 @@ class PatientTest {
   public void newPatient_instantiatesWithPassword(Patient patient) {
     assertEquals("12345678", patient.getPassword());
   }
+
+  @Test
+  @DisplayName("Test that a patient's id is set as specified")
+  public void setId_setsPatientId_true(Patient patient) {
+    patient.setId(1);
+    assertEquals(1, patient.getId());
+  }
+
+  @Test
+  @DisplayName("Test that a patient's name is set as specified")
+  public void setName_setsPatientName_true(Patient patient) {
+    patient.setName("Jenny Doe");
+    assertEquals("Jenny Doe", patient.getName());
+  }
+
+  @Test
+  @DisplayName("Test that a patient's email is set as specified")
+  public void setPhone_setsPatientPhoneNo_true(Patient patient) {
+    patient.setPhone(711808960);
+    assertEquals(711808960, patient.getPhone());
+  }
+
+  @Test
+  @DisplayName("Test that a patient's address is set as specified")
+  public void setAddress_setsPatientAddress_true(Patient patient) {
+    patient.setAddress("Lavington, Nairobi");
+    assertEquals("Lavington, Nairobi", patient.getAddress());
+  }
+
+  @Test
+  @DisplayName("Test that a patient's email is set as specified")
+  public void setEmail_setsPatientEmail_true(Patient patient) {
+    patient.setEmail("jennydoe@gmail.com");
+    assertEquals("jennydoe@gmail.com", patient.getEmail());
+  }
+
+  @Test
+  @DisplayName("Test that a patient's password is set as specified")
+  public void setPassword_setsPatientPassword_true(Patient patient) {
+    patient.setPassword("123456789");
+    assertEquals("123456789", patient.getPassword());
+  }
 }
