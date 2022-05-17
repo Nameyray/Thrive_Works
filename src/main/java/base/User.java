b9d113297fdda34aaac993166847695af828e62d
Nameyray <eparis@atlassian.com>
2022-05-17 15:56:36 +0300

    create model class User

---
+++ src/main/java/base/User.java
@@ -0,0 +1,58 @@
+package base;
+
+public abstract class User {
+    protected int id;
+    protected String name;
+    protected int phone;
+    protected String email;
+    protected String password;
+    protected String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
