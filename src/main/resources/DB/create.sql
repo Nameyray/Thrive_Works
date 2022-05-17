9c26e0597528cbf65c2760a321e98f70b79f78bf
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-17 15:35:23 +0300

    Update database structure

---
+++ src/main/resources/DB/create.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE thrive;
+
+\c thrive;
+
+CREATE TABLE users(
+id serial PRIMARY KEY,
+name VARCHAR,
+phone INT,
+address VARCHAR,
+email VARCHAR,
+password VARCHAR,
+specialization VARCHAR,
+ratings INT,
+is_vetted INT
+);
+
+CREATE TABLE appointments(
+id serial PRIMARY KEY,
+starttime DATE,
+createdat TIMESTAMP,
+endtime DATE,
+patient INT,
+therapist INT,
+link VARCHAR
+);
+
+CREATE TABLE reviews(
+  id serial PRIMARY KEY,
+  appointmentid INT,
+  feedback VARCHAR
+);
+
+CREATE TABLE payments(
+id serial PRIMARY KEY,
+appointmentid INT,
+amount DOUBLE PRECISION
+);
+
+CREATE DATABASE thrive_test WITH TEMPLATE thrive;
--- src/main/resources/DB/drop.sql
+++ src/main/resources/DB/drop.sql
@@ -1,25 +1,2 @@
-CREATE DATABASE thrive;
-
-\c thrive;
-
-CREATE TABLE users(
-id serial PRIMARY KEY,
-name VARCHAR,
-phone INT,
-address VARCHAR,
-email VARCHAR,
-password VARCHAR,
-specialization VARCHAR,
-ratings INT,
-is_vetted INT
-
-);
-
-CREATE TABLE appointments(
-id serial PRIMARY KEY,
-starttime DATE,
-createdat TIMESTAMP,
-endtime DATE,
-patient INT,
-
-);
+DROP DATABASE thrive;
+DROP DATABASE thrive_test;
