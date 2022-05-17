565d07c2f4dc705865760c77a9f66802ea7bfb43
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-17 20:50:04 +0300

    Add parameter resolver for dependency injection

---
+++ src/test/java/parameter_resolver/TherapistParameterResolver.java
@@ -0,0 +1,19 @@
+package parameter_resolver;
+
+import models.Therapist;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class TherapistParameterResolver implements ParameterResolver {
+  @Override
+  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return parameterContext.getParameter().getType() == Therapist.class;
+  }
+
+  @Override
+  public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return new Therapist("Anxiety disorders", "John Doe", 723098123, "5th floor, Ngong lane plaza", "jdoe@gmail.com", "12345678");
+  }
+}
