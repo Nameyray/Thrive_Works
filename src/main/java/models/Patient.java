94f396dd1128793ab2d7efc3b784c11639727537
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-17 23:35:16 +0300

    Update Patient model class

--- src/main/java/models/Patient.java
+++ src/main/java/models/Patient.java
@@ -3,11 +3,13 @@ package models;
 import base.User;
 
 public class Patient extends User {
+    private static final String USER_ROLE = "Patient";
     public Patient(String name, int phone, String address, String email, String password) {
         this.name = name;
         this.phone = phone;
         this.address = address;
         this.email = email;
         this.password = password;
+        this.role = USER_ROLE;
     }
 }
